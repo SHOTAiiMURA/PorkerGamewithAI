e22206c1fd2ade7eaacae32bc9811c145fada75c
SHOTAiiMURA <hn.j.shota027503@gamil.com>
2024-06-25 22:49:11 +0900

    create if there are one pair of card.

--- src/poker/PlayerHandImp.java
+++ src/poker/PlayerHandImp.java
@@ -1,6 +1,8 @@
 package poker;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class PlayerHandImp extends HandImp implements PlayerHand{
@@ -20,6 +22,8 @@ public class PlayerHandImp extends HandImp implements PlayerHand{
 
         // check one pair
         Set<Integer> result_pairs = new HashSet<>();
+        List<Integer> result_three_of_a_kind = new ArrayList<>();
+        List<Integer> result_four_of_a_kind = new ArrayList<>();
         int counter = 0;
         for (Card card1 : combinedHand.getCards()) {
             for(Card card2 : combinedHand.getCards()) {
@@ -39,9 +43,33 @@ public class PlayerHandImp extends HandImp implements PlayerHand{
             isTwoPair = true;
         }
         // check three of a kind
-
+        for (Card card1 : combinedHand.getCards()) {
+            for(Card card2 : combinedHand.getCards()) {
+                if(card1.getNumber()==card2.getNumber()) {
+                    counter += 1;
+                }
+            }
+            if (counter ==3){
+                result_pairs.add(card1.getNumber());
+            }
+        }
+        if (result_pairs.size()>=3){
+            isThreeOfAKind = true;
+        }
         // check four of a kind
-
+        for (Card card1 : combinedHand.getCards()) {
+            for(Card card2 : combinedHand.getCards()) {
+                if(card1.getNumber()==card2.getNumber()) {
+                    counter += 1;
+                }
+            }
+            if (counter ==4){
+                result_pairs.add(card1.getNumber());
+            }
+        }
+        if (result_pairs.size()>=4){
+            isFourOfAKind = true;
+        }
         // check straight
 
         // check flush
